db891f4020ef70eae821b55b0df8808ed1bda369
sures <sures@LAPTOP-4M6LAJK0>
2021-05-11 11:26:27 -0400

    tester   ini
    Signed-off-by: practicet20@gmail.com <practicet20@gmail.com>

---
+++ src/myrediffpractice/rediffguipractice.java
@@ -0,0 +1,12 @@
+
+package myrediffpractice;
+
+public class rediffguipractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("this is from gui");
+
+	}
+
+}
