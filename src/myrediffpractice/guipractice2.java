0295b77a7822c06be1129757107deef83526c64a
practicet20@gmail.com <practicet20@gmail.com>
2021-05-12 11:05:42 -0400

    Signed-off-by: practicet20@gmail.com <practicet20@gmail.com>

---
+++ src/myrediffpractice/guipractice2.java
@@ -0,0 +1,11 @@
+package myrediffpractice;
+
+public class guipractice2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("gui practice");
+
+	}
+
+}
